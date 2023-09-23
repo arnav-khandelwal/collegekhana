1d870358c7901c4bf83c1ba0566a35506921bd02
Arnav Khandelwal <arnav181104@gmail.com>
2023-09-23 20:30:49 +0530

    commit new

--- app/src/main/java/com/example/collegekhana/MainMenu.java
+++ app/src/main/java/com/example/collegekhana/MainMenu.java
@@ -3,12 +3,23 @@ package com.example.collegekhana;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainMenu extends AppCompatActivity {
+
+    Button signinemail,signinphone,signup;
+    ImageView bgimage;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu2);
+        setContentView(R.layout.activity_main_menu);
+
+        final Animation Zoomin = AnimationUtils.loadAnimation(this,R.anim.)
     }
 }
