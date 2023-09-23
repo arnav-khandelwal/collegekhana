572bdaf02493bea2f9a095070a36804b34b358c3
Arnav Khandelwal <arnav181104@gmail.com>
2023-09-23 15:53:31 +0530

    commit after java

---
+++ app/src/main/java/com/example/collegekhana/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.collegekhana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView)findViewById(R.id.imageView);
+        textView = (TextView)findViewById(R.id.textView7);
+
+        imageView.animate().alpha(0f).setDuration(0);
+        textView.animate().alpha(0f).setDuration(0);
+
+    }
+}
