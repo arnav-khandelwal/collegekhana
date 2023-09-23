3f81cb885a52dccaec20683bbbf9f27993b7a29f
Arnav Khandelwal <arnav181104@gmail.com>
2023-09-23 21:26:30 +0530

    commit 5

---
+++ app/src/main/java/com/example/collegekhana/ChooseOne.java
@@ -0,0 +1,69 @@
+package com.example.collegekhana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ChooseOne extends AppCompatActivity {
+    Button Chef,Customer,DeliveryPerson;
+    String type;
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_one);
+
+        intent = getIntent();
+        type = intent.getStringExtra("Home").toString().trim();
+
+        Chef = (Button) findViewById(R.id.chef);
+        Customer = (Button) findViewById(R.id.customer);
+
+
+        Chef.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (type.equals("Email")) {
+                    Intent loginemail = new Intent(ChooseOne.this, Cheflogin.class);
+                    startActivity(loginemail);
+                    finish();
+                }
+                if (type.equals("Phone")) {
+                    Intent loginphone = new Intent(ChooseOne.this, Chefloginphone.class);
+                    startActivity(loginphone);
+                    finish();
+                }
+                if (type.equals("SignUp")) {
+                    Intent Register = new Intent(ChooseOne.this, ChefRegistration.class);
+                    startActivity(Register);
+                }
+            }
+        });
+
+        Customer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (type.equals("Email")) {
+                    Intent loginemailcust = new Intent(ChooseOne.this, Login.class);
+                    startActivity(loginemailcust);
+                    finish();
+                }
+                if (type.equals("Phone")) {
+                    Intent loginphonecust = new Intent(ChooseOne.this, Loginphone.class);
+                    startActivity(loginphonecust);
+                    finish();
+                }
+                if (type.equals("SignUp")) {
+                    Intent Registercust = new Intent(ChooseOne.this, Registration.class);
+                    startActivity(Registercust);
+                }
+
+            }
+        });
+    }
+    }
--- app/src/main/java/com/example/collegekhana/MainMenu.java
+++ app/src/main/java/com/example/collegekhana/MainMenu.java
@@ -2,7 +2,9 @@ package com.example.collegekhana;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
@@ -13,13 +15,49 @@ public class MainMenu extends AppCompatActivity {
     Button signinemail,signinphone,signup;
     ImageView bgimage;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-        final Animation Zoomin = AnimationUtils.loadAnimation(this,R.anim.)
+        signinemail=(Button)findViewById(R.id.SignwithEmail);
+        signinphone=(Button)findViewById(R.id.signwithPhone);
+        signup=(Button)findViewById(R.id.SignUp);
+
+        signinemail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signemail = new Intent(MainMenu.this,ChooseOne.class);
+                signemail.putExtra("Home","Email");
+                startActivity(signemail);
+                finish();
+            }
+        });
+
+        signinphone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signphone = new Intent(MainMenu.this,ChooseOne.class);
+                signphone.putExtra("Home","Phone");
+                startActivity(signphone);
+                finish();
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signup = new Intent(MainMenu.this,ChooseOne.class);
+                signup.putExtra("Home","SignUp");
+                startActivity(signup);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.gc();
     }
 }
