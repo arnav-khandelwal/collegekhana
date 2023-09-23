3ab600dde76fc647574b260ae040563a333407c2
ARYANRAJ1121 <112928666+ARYANRAJ1121@users.noreply.github.com>
2023-09-23 17:48:55 +0530

    arnav2.0

--- app/src/main/java/com/example/collegekhana/MainActivity.java
+++ app/src/main/java/com/example/collegekhana/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.collegekhana;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,6 +26,23 @@ public class MainActivity extends AppCompatActivity {
 
         imageView.animate().alpha(0f).setDuration(0);
         textView.animate().alpha(0f).setDuration(0);
+        imageView.animate().alpha(1f).setDuration (1000).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                textView.animate().alpha(1f).setDuration(800);
+            }
+        });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(MainActivity.this,MainMenu.class);
+                startActivity(intent);
+                finish();
+
+
+            }
+        }, 3000);
 
     }
 }
